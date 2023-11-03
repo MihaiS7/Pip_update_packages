afb553375107607d8ea5a7b11a1a4c39ce2d592b
Toretto99 <mihaiseles7@gmail.com>
2023-11-03 11:51:04 +0100

    Saving correctly to the csv file

--- requests_test.py
+++ requests_test.py
@@ -3,26 +3,26 @@ import veryfing_updating
 import subprocess
 import csv
 
-if __name__ == "__main__":
-    packages_lines = veryfing_updating.verifying("requirements.txt")
+with open("checking_dates.csv", "w", encoding='UTF8') as csvfile: # Write the data to a csv file
+    writer = csv.writer(csvfile)
+    writer.writerow(["Package", "Date"])
+
+    if __name__ == "__main__":
+        packages_lines = veryfing_updating.verifying("requirements.txt")
 
 
-for package in packages_lines:
-    package = package.strip()
-    print(package)
-    r = requests.get(f"https://pypi.org/pypi/{package}/json")
-    data = r.json()
+    for package in packages_lines:
+        package = package.strip()
+        r = requests.get(f"https://pypi.org/pypi/{package}/json")
+        data = r.json()
 
-    finding = [y for y in data.values()][-2]
+        finding = [y for y in data.values()][-2] # Getting the [-2] list of values from the data requested
 
-    date = [x['upload_time'] for x in finding if 'upload_time' in x][0]
-    print(date[:10])
-    print(f"{package} \n{date}")
+        date = [x['upload_time'] for x in finding if 'upload_time' in x][0][:10] # Get the first data in format (Y/M/D)
+        print(f"{[package]} \n{date}")
 
-    with open("checking_dates.csv", "w", encoding='UTF8') as csvfile:
-        writer = csv.writer(csvfile)
-        writer.writerow(["Package", "Date"])
-        writer.writerows([date])
+    
+        writer.writerow([package, date])
 
 
-subprocess.run(["open", "checking_dates.csv"])
+subprocess.run(["open", "checking_dates.csv"]) # Open the csv file when is done
