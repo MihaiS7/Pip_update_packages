36c5311da13dc3aead07b36a0484638e2aeb2fc4
Toretto99 <117114759+Toretto99@users.noreply.github.com>
2023-11-05 17:05:05 +0100

    Adding more comments and also making progress

--- requests_test.py
+++ requests_test.py
@@ -10,8 +10,8 @@ if __name__ == "__main__":
 
 with open("checking_dates.csv", "w", encoding='UTF8') as csvfile: # Write the data to a csv file
     writer = csv.writer(csvfile)
-    writer.writerow(["Package", "Date"]) # Header
-    result = []
+    writer.writerow(["Package", "Date"]) # Header of the file
+    result = {}
     for package in packages_lines:
         package = package.strip() # Strip the \n
         r = requests.get(f"https://pypi.org/pypi/{package}/json")
@@ -22,12 +22,15 @@ with open("checking_dates.csv", "w", encoding='UTF8') as csvfile: # Write the da
         date = [x['upload_time'] for x in finding if 'upload_time' in x][0][:10] # Get the first data in format (Y/M/D)
         # print(f"{package} : {date}")
         sorting_date = datetime.date.fromisoformat(date).strftime("%Y-%m-%d")
-        result.append(sorting_date)
-        print(sorted(result))
-        # writer.writerow([package, date])
+        result[package]=sorting_date # Creating the dict - package-key, sorting_date-value
+        sorted_dict = sorted(result.items(), key=lambda item: item[1]) # Sorting the values
+# print(sorted_dict)
+        for package_date in sorted_dict:
+                print(package_date)
+                writer.writerow([package_date])
+        
 
-
-# subprocess.run(["open", "checking_dates.csv"]) # Open the csv file when is done
+subprocess.run(["open", "checking_dates.csv"]) # Open the csv file when is done
 
 
                 
