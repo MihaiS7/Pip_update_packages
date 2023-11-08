edf77309eef7348ddbe05374047b0e97803377e3
Toretto99 <117114759+Toretto99@users.noreply.github.com>
2023-11-08 17:09:02 +0100

    modifying the gitignore

--- requests_test.py
+++ requests_test.py
@@ -3,7 +3,8 @@ import veryfing_updating
 import subprocess
 import csv
 import datetime
-        
+
+print("The script started!!!")
 
 if __name__ == "__main__":
     packages_lines = veryfing_updating.verifying("requirements.txt")
@@ -12,8 +13,9 @@ def writing_file(file):
         writer = csv.writer(csvfile)
         writer.writerow(["Package", "Date"]) # Header of the file
         result = {}
-        for package in packages_lines:
+        for package in packages_lines[:]:
             package = package.strip() # Strip the \n
+            print(f"This is the package: {package}")
             r = requests.get(f"https://pypi.org/pypi/{package}/json")
             data = r.json()
 
@@ -22,22 +24,37 @@ def writing_file(file):
             date = [x['upload_time'] for x in finding if 'upload_time' in x][0][:10] # Get the first data in format (Y/M/D)
             # print(f"{package} : {date}")
             sorting_date = datetime.datetime.strptime(date,"%Y-%m-%d").strftime("%Y %m")
-            result[package]=date # Creating the dict - package-key, sorting_date-value
+            result[package]=sorting_date # Creating the dict - package-key, sorting_date-value
             sorted_dict = sorted(result.items(), key=lambda item: item[1]) # Sorting the values
     # print(sorted_dict)
         for package_date in sorted_dict:
-            pack, da = (package_date)
+            pack, date_format = (package_date)
 
-            if sorting_date <= today_year_month():
-                print(pack, da)
-                writer.writerow([pack, da])
+            if you_set_date('2022-01') <= date_format <= today_year_month():
+                print(pack, date_format)
+                writer.writerow([pack, date_format])
+            else:
+                #Delete package
+                try:
+                    print(f"Package deleted: {pack}")
+                    packages_lines.remove(package)
+                except ValueError as e:
+                    print(f"I encountered an error: {e}")
+                    
+                
+                
+                
 
 def today_year_month():
     today = datetime.datetime.today().strftime("%Y %m")
     return today
 
+def you_set_date(year_month):
+    return datetime.datetime.strptime(year_month, "%Y-%m").strftime("%Y %m")
+
+# you_set_date('2022-01')
 writing_file("checking_dates.csv")
-print(today_date())
+print(f"Today date: {today_year_month()}")
 
 # subprocess.run(["open", "checking_dates.csv"]) # Open the csv file when is done
 
